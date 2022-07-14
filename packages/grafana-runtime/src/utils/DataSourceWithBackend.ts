ad5165d74684a37a31b4bc0a7557185a7b8841b5
Darren Janeczek <darren.janeczek@grafana.com>
2022-07-13 23:41:02 -0400

    Reverse a change that become unnecessary

--- packages/grafana-runtime/src/utils/DataSourceWithBackend.ts
+++ packages/grafana-runtime/src/utils/DataSourceWithBackend.ts
@@ -219,21 +219,14 @@ class DataSourceWithBackend<
    * Make a GET request to the datasource resource path
    */
   async getResource(path: string, params?: any): Promise<any> {
-    return getBackendSrv().get(this.getResourcePath(path), params);
+    return getBackendSrv().get(`/api/datasources/${this.id}/resources/${path}`, params);
   }
 
   /**
    * Send a POST request to the datasource resource path
    */
   async postResource(path: string, body?: any): Promise<any> {
-    return getBackendSrv().post(this.getResourcePath(path), { ...body });
-  }
-
-  /**
-   * Get the datasource resource path for the given path
-   */
-  getResourcePath(path: string): string {
-    return `/api/datasources/${this.id}/resources/${path}`;
+    return getBackendSrv().post(`/api/datasources/${this.id}/resources/${path}`, { ...body });
   }
 
   /**
